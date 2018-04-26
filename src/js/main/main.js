7ed4ad873925c0d4e373d460b9c8e808779e0190
Artur <arko8919@gmail.com>
2018-04-26 21:32:31 +0100

    boilerplate and added base html, css, file project structure, AJAX GET request

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -9,6 +9,20 @@ export class Main extends React.Component {
         this.state = {}
     }
 
+    componentDidMount(){
+        const xhr = new XMLHttpRequest();
+        const url = 'https://api.instagram.com/v1/users/self/?access_token=' +
+            '7600268179.4606266.0a0e1c6cf9f04bb38c9e7362a4880a98';
+        xhr.dataType = "jsonp";
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                console.log(xhr.response);
+            }
+        };
+        xhr.open("GET", url);
+        xhr.send();
+    }
+
     render() {
         return (
             <div className="main">
